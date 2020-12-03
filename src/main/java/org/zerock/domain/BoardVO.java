b6e5b4ffeb872128dcf50faf2d4c69c5cebf61d8
hazzang777 <68279162+hazzang777@users.noreply.github.com>
2020-12-03 12:31:24 +0900

    스프링을 이용한 나만의 게시판 - HAZZANG

---
+++ src/main/java/org/zerock/domain/BoardVO.java
@@ -0,0 +1,20 @@
+package org.zerock.domain;
+
+import java.sql.Date;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private long bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private Date updatedate;
+	
+	private int replyCnt;
+	
+	private List<BoardAttachVO> attachList;
+}
